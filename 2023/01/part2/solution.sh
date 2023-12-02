d65c315a45b7e741ab039871c46feccbdfee4c5a
panJarda <jaroslav.pernica9@gmail.com>
2023-12-02 12:20:27 +0100

    fix

--- 2023/01/part2/solution.sh
+++ 2023/01/part2/solution.sh
@@ -1,6 +1,6 @@
 sed -r 's/(one|two|three|four|five|six|eight|ninstarte)/*&*' input.txt > in
 rev input.txt | sed -r 's/(eno|owt|eerht|ruof|evif|xis|thgie|enin)/*&*/' | rev > inend
-cat estart | ed instart
+cat edstart | ed instart
 cat edend | ed inend
 paste -d'\0' start end > out
 cat out | xargs | dc -e '? 0d [+ z1<a]dsax p'
